d0eec0dd3cfa49b23043edf507c5968513148180
rfalefp <rafaelfepedrosa@gmail.com>
2021-12-06 18:16:33 -0300

    Modal

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,11 +1,52 @@
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { createServer } from 'miragejs'
+import Modal from 'react-modal'
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+    
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1, 
+          title: 'Transaction 1',
+          amount: 'deposit',
+          category: 'Food',
+          createdAt: new Date(),
+        }
+      ]
+    })
+  }
+})
+
+Modal.setAppElement('#root')
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+  
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true)
+  }
+  
+  function handleCloseNewTransactionModal() {
+      setIsNewTransactionModalOpen(false)
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />    
+      
       <GlobalStyle />
     </>
   )
